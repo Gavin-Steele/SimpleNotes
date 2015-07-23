b76054261c319fa78f7368ba8a36892a5dc424a2
gsteele <gsteele@bonline.com>
2015-07-23 14:30:28 +0100

    [First Build]

---
+++ www/js/app.js
@@ -0,0 +1,83 @@
+(function(){
+
+var app = angular.module('notes', ['ionic', 'notes.notestore']);
+
+app.config(function($stateProvider, $urlRouterProvider) {
+
+  $stateProvider.state('list', {
+    url: '/list',
+    templateUrl: 'views/list.html'
+  });
+    $stateProvider.state('edit', {
+    url: '/edit/:noteId',
+    templateUrl: 'views/edit.html',
+    controller: 'EditCtrl'
+  });
+    $stateProvider.state('add', {
+    url: '/add',
+    templateUrl: 'views/edit.html',
+    controller: 'AddCtrl'
+  });
+
+  $urlRouterProvider.otherwise('/list');
+});
+
+app.controller('ListCtrl', function($scope, NoteStore) {
+
+  $scope.reorder = false;
+
+  $scope.notes = NoteStore.list();
+  $scope.remove = function (noteId) {
+    NoteStore.remove(noteId);
+  };
+
+  $scope.move = function(note, fromIndex, toIndex){
+    NoteStore.move(note, fromIndex, toIndex);
+  };
+
+  $scope.toggleReordering =function(){
+    $scope.reordering = !$scope.reordering;
+  };
+
+
+
+});
+
+app.controller('AddCtrl', function($scope, $state, NoteStore){
+  
+  $scope.note = {
+    id: new Date().getTime().toString(),
+    title: '',
+    description: '',
+    pictureUrl: ''
+  };
+
+  $scope.save = function(){
+    NoteStore.create($scope.note);
+    $state.go('list');
+  };
+});
+
+app.controller('EditCtrl', function($scope, $state, NoteStore){
+  
+  $scope.note = angular.copy(NoteStore.get($state.params.noteId));
+
+  $scope.save = function(){
+    NoteStore.update($scope.note);
+    $state.go('list');
+  };
+});
+
+app.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+});
+}());
